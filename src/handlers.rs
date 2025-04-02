e404875dbd7c88a4819e0e5afbafc35a226ac445
Harsh Ranjan <harsh150997@gmail.com>
2025-04-02 20:01:13 +0530

    before encapsulating routes

--- src/handlers.rs
+++ src/handlers.rs
@@ -6,7 +6,6 @@ use axum::{
     http::{HeaderMap, StatusCode, header},
     response::IntoResponse,
 };
-use log::{error, info};
 
 pub async fn home() -> impl IntoResponse {
     let mut headers = HeaderMap::new();
@@ -16,18 +15,25 @@ pub async fn home() -> impl IntoResponse {
     match body.render() {
         Ok(b) => (StatusCode::OK, headers, b),
         Err(e) => {
-            error!("could not render template home.html : {}", e);
-            (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                headers,
-                format!("could not render template home.html : {}", e),
-            )
+            tracing::error!("could not render template home.html : {}", e);
+            server_error()
         }
     }
 }
 
+fn server_error() -> (StatusCode, HeaderMap, String) {
+    let mut headers = HeaderMap::new();
+    headers.insert(header::SERVER, "Rust".parse().unwrap());
+    headers.insert(header::CONTENT_TYPE, "html".parse().unwrap());
+    (
+        StatusCode::INTERNAL_SERVER_ERROR,
+        headers,
+        format!("could not render template home.html : {}", e),
+    )
+}
+
 pub async fn snippet_view(Path(snippet_id): Path<u32>) -> String {
-    info!("{snippet_id}");
+    tracing::info!("{snippet_id}");
     format!(
         "Display a specific snippet with a specific id : {}",
         snippet_id
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,59 @@
 mod handlers;
 mod templates;
 use crate::handlers::{home, snippet_create, snippet_create_post, snippet_view};
-// use crate::handlers;
 use axum::{
     Router,
-    extract::Path,
-    http::{
-        StatusCode, Uri,
-        header::{self, HeaderMap, HeaderName},
-    },
-    response::IntoResponse,
-    routing::{get, post},
+    http::Request,
+    routing::{get, get_service, post},
 };
+use clap::Parser;
+use tower::ServiceExt;
+use tower_http::{services::ServeDir, trace::TraceLayer};
 // for trailing slash routes
 //https://github.com/tokio-rs/axum/issues/1118
 use axum_extra::routing::RouterExt;
 
-use env_logger::Env;
-use log::{Level, debug, error, info, log_enabled};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-fn init_logger() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// port
+    #[arg(short, long)]
+    port: u16,
 }
 
 #[tokio::main]
 async fn main() {
-    init_logger();
+    // init_logger();
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    let args = Args::parse();
     // our router
     let app = Router::new()
         .route("/", get(home))
         .route_with_tsr("/snippet/view/{id}", get(snippet_view))
         .route_with_tsr("/snippet/create", get(snippet_create))
-        .route("/snippet/create", post(snippet_create_post));
+        .route("/snippet/create", post(snippet_create_post))
+        .nest_service("/static", ServeDir::new("static"))
+        .layer(TraceLayer::new_for_http());
 
-    let listener_res = tokio::net::TcpListener::bind("0.0.0.0:4000").await;
+    let listener_res = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", args.port)).await;
     let listener = match listener_res {
         Ok(l) => l,
         Err(e) => {
-            error!("was not able to start server : {}", e);
+            tracing::error!("was not able to start server : {}", e);
             return;
         }
     };
-    info!("server starting on :4000");
+    tracing::info!("server starting on :{}", args.port);
     axum::serve(listener, app)
         .await
-        .unwrap_or_else(|e| error!("was not able to start server : {}", e));
+        .unwrap_or_else(|e| tracing::error!("was not able to start server : {}", e));
 }
